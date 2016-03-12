b0a075556e03496eefb988c34d9ab8bb73679537
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 15:22:07 +0200

    First test for 22 mushrooms

---
+++ RedMushroom/RedMushroom/RedMushrooms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedMushrooms
+{
+    [TestClass]
+    public class RedMushrooms
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int RedMushrooms = CalculateRedMushrooms(22, 10);
+            Assert.AreEqual(20, RedMushrooms);
+        }
+        int CalculateRedMushrooms(int TotalMushrooms, int MushroomDifference)
+        {
+            return TotalMushrooms*MushroomDifference/(MushroomDifference+1);
+        }
+    }
+}
