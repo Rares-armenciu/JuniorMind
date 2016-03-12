f90c4d79cd922674e875b599952916e8824a7750
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 16:06:51 +0200

    Test for first case
    4x4 stone for 4x4 pavement

--- StreetPavement/StreetPavement/StreetPavement.cs
+++ StreetPavement/StreetPavement/StreetPavement.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace StreetPavement
 {
     [TestClass]
-    public class UnitTest1
+    public class StreetPavement
     {
         [TestMethod]
         public void TestMethod1()
         {
+            int Result = CalculateNeededNumberOfCubicalStone(4, 4, 4);
+            Assert.AreEqual(1, Result);
+        }
+        int CalculateNeededNumberOfCubicalStone(int StreetLength, int StreetWidth, int CubicalStone)
+        {
+            int Length = StreetLength / CubicalStone;
+            int Width = StreetWidth / CubicalStone;
+            return Length*Width;
         }
     }
 }
