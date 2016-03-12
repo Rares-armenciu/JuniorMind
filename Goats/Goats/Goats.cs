014a2339792b3f2578725f5677d87156fc37840d
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 16:37:09 +0200

    Second test, for a more complex case

--- Goats/Goats/Goats.cs
+++ Goats/Goats/Goats.cs
@@ -12,7 +12,13 @@ namespace Goats
             decimal amountOfHay = CalculateKilogrammsOfHay(1, 1, 3, 2, 1);
             Assert.AreEqual(6, amountOfHay);
         }
-        decimal CalculateKilogrammsOfHay(int noOfDays, int noOfGoats, decimal kgOfHay, int goats, int days)
+        [TestMethod]
+        public void TestMethod2()
+        {
+            decimal amountOfHay = CalculateKilogrammsOfHay(2, 2, 5, 4, 4);
+            Assert.AreEqual(20, amountOfHay);
+        }
+        decimal CalculateKilogrammsOfHay(int noOfDays, int noOfGoats, decimal kgOfHay, int days, int goats)
         {
             decimal hayPerGoatPerDay = kgOfHay / (noOfDays * noOfGoats);
             return hayPerGoatPerDay*goats*days;
