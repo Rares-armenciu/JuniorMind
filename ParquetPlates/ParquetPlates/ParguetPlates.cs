57afe6e45c4b78ddc13a86b1b14c5d7d4ea7c799
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 22:51:15 +0200

    Second test, for a more complex case

--- ParquetPlates/ParquetPlates/ParguetPlates.cs
+++ ParquetPlates/ParquetPlates/ParguetPlates.cs
@@ -12,6 +12,12 @@ namespace ParquetPlates
             double numberOfPlates = GetNumberOfNeededPlates(3.4, 2, 1, 2);
             Assert.AreEqual(4, numberOfPlates);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double numberOfPlates = GetNumberOfNeededPlates(51, 10, 4, 3);
+            Assert.AreEqual(50, numberOfPlates);
+        }
         double GetNumberOfNeededPlates(double roomLength, double roomWidth, double plateLength, double plateWidth)
         {
             return (roomLength * roomWidth) / (plateLength * plateWidth * 0.85);
