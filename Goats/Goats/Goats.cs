86b843ec86c78ed2cfbe34b52f75e607736ae6f7
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 16:35:36 +0200

    First test for a simple case

---
+++ Goats/Goats/Goats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goats
+{
+    [TestClass]
+    public class Goats
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal amountOfHay = CalculateKilogrammsOfHay(1, 1, 3, 2, 1);
+            Assert.AreEqual(6, amountOfHay);
+        }
+        decimal CalculateKilogrammsOfHay(int noOfDays, int noOfGoats, decimal kgOfHay, int goats, int days)
+        {
+            decimal hayPerGoatPerDay = kgOfHay / (noOfDays * noOfGoats);
+            return hayPerGoatPerDay*goats*days;
+        }
+    }
+}
