81be558b534b5c830e3688e4e70b0bf78fd1034b
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 17:06:11 +0200

    First test for a simple case

--- SportRounds/SportRounds/SportRepetitions.cs
+++ SportRounds/SportRounds/SportRepetitions.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SportRounds
 {
     [TestClass]
-    public class UnitTest1
+    public class SportRepetitions
     {
         [TestMethod]
         public void TestMethod1()
         {
+            int noOfRepetitions = CalculateTotalNoOfRepetitions(3);
+            Assert.AreEqual(9, noOfRepetitions);
+        }
+        int CalculateTotalNoOfRepetitions(int rounds)
+        {
+            return (rounds-1)*(rounds)+rounds;
         }
     }
 }
