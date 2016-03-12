cb00121f7a8a79338a75cb95dafdb044830c04b6
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 17:08:00 +0200

    Second test, for a more complex case

--- SportRounds/SportRounds/SportRepetitions.cs
+++ SportRounds/SportRounds/SportRepetitions.cs
@@ -12,6 +12,12 @@ namespace SportRounds
             int noOfRepetitions = CalculateTotalNoOfRepetitions(3);
             Assert.AreEqual(9, noOfRepetitions);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int noOfRepetitions = CalculateTotalNoOfRepetitions(10);
+            Assert.AreEqual(100, noOfRepetitions);
+        }
         int CalculateTotalNoOfRepetitions(int rounds)
         {
             return (rounds-1)*(rounds)+rounds;
