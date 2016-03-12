1978c6ed27a3479b7133870aa21718cff36ef9a9
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-12 16:14:21 +0200

    Second test, to cover more cases

--- StreetPavement/StreetPavement/StreetPavement.cs
+++ StreetPavement/StreetPavement/StreetPavement.cs
@@ -12,11 +12,21 @@ namespace StreetPavement
             int Result = CalculateNeededNumberOfCubicalStone(4, 4, 4);
             Assert.AreEqual(1, Result);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int Result = CalculateNeededNumberOfCubicalStone(6, 6, 4);
+            Assert.AreEqual(4, Result);
+        }
         int CalculateNeededNumberOfCubicalStone(int StreetLength, int StreetWidth, int CubicalStone)
         {
             int Length = StreetLength / CubicalStone;
             int Width = StreetWidth / CubicalStone;
-            return Length*Width;
+            if (StreetLength % CubicalStone > 0)
+                Length += 1;
+            if (StreetWidth % CubicalStone > 0)
+                Width += 1;
+            return Length *Width;
         }
     }
 }
