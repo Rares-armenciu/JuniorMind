2e86f6b127b68d82bc12215df10cc3b2a7247ff9
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-13 19:09:09 +0200

    Test for a simple example

--- FlyingBird/FlyingBird/FlyingBird.cs
+++ FlyingBird/FlyingBird/FlyingBird.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FlyingBird
 {
     [TestClass]
-    public class UnitTest1
+    public class FlyingBird
     {
         [TestMethod]
         public void TestMethod1()
         {
+            float distance = CalculateFlyDistance(20, 80);
+            Assert.AreEqual(40, distance);
+        }
+        float CalculateFlyDistance(float trainSpeed, float trainDistance)
+        {
+            float birdTravelTime = trainDistance / (trainSpeed * 2) - ((trainDistance / 4) / trainSpeed);
+            return trainSpeed*2*birdTravelTime;
         }
     }
 }
