62475cc67d0737d80dd1a823b7e70e1a292ea2cc
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-13 16:30:48 +0200

    Test for an easy example

--- BuildingArea/BuildingArea/BuildingArea.cs
+++ BuildingArea/BuildingArea/BuildingArea.cs
@@ -9,6 +9,12 @@ namespace BuildingArea
         [TestMethod]
         public void TestMethod1()
         {
+            decimal areaOfBuilding = CalculateAreaOfBuilding(0, 0, 2, 0,1 , 2);
+            Assert.AreEqual(2, areaOfBuilding);
+        }
+        decimal CalculateAreaOfBuilding(decimal x1, decimal y1, decimal x2, decimal y2, decimal x3, decimal y3)
+        {
+            return (x1 * y2 + x2 * y3 + x3 * y1 - x3 * y2 - x2 * y1 - x1 * y3) / 2;
         }
     }
 }
