7cd02fab71c5cecfd814e9b1332fe6edc287a638
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-17 13:31:16 +0200

    Watermelon

--- Watermelon/Watermelon/Watermelon.cs
+++ Watermelon/Watermelon/Watermelon.cs
@@ -4,11 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Watermelon
 {
     [TestClass]
-    public class UnitTest1
+    public class Watermelon
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(true, CheckWatermelonKg(20));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, CheckWatermelonKg(21));
+        }
+        Boolean CheckWatermelonKg(int watermelonWeight)
+        {
+            if (watermelonWeight % 2 == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
