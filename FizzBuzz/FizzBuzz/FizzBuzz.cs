bf0fd369d6f8d710722b140a475089985ed8721f
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-17 13:40:46 +0200

    Corrected code to work for FizzBuzz

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -18,12 +18,12 @@ namespace FizzBuzz
         }
         string CheckFizzBuzz(int number)
         {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
             if (number % 3 == 0)
                 return "Fizz";
             if (number % 5 == 0)
                 return "Buzz";
-            if (number % 3 == 0 && number % 5 == 0)
-                return "FizzBuzz";
             else
                 return "No FizzBuzz";
         }
