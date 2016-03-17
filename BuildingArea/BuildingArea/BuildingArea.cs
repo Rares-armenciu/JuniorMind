2fb125c8e1e8367e6d3c1a75d74c342f38ce4cbb
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-17 13:24:05 +0200

    Improved BuildingArea

--- BuildingArea/BuildingArea/BuildingArea.cs
+++ BuildingArea/BuildingArea/BuildingArea.cs
@@ -9,16 +9,16 @@ namespace BuildingArea
         [TestMethod]
         public void TestMethod1()
         {
-            decimal areaOfBuilding = CalculateAreaOfBuilding(0, 0, 2, 0, 1 , 2);
+            float areaOfBuilding = CalculateAreaOfBuilding(0, 0, 2, 0, 1 , 2);
             Assert.AreEqual(2, areaOfBuilding);
         }
         [TestMethod]
         public void TestMethod2()
         {
-            decimal areaOfBuilding = CalculateAreaOfBuilding(2, 1, 4, 5, 2, 3);
-            Assert.AreEqual(2, areaOfBuilding);
+            float areaOfBuilding = CalculateAreaOfBuilding(0, 0, 1, 0, 0, 1);
+            Assert.AreEqual(0.5, areaOfBuilding);
         }
-        decimal CalculateAreaOfBuilding(decimal x1, decimal y1, decimal x2, decimal y2, decimal x3, decimal y3)
+        float CalculateAreaOfBuilding(float x1, float y1, float x2, float y2, float x3, float y3)
         {
             return (x1 * y2 + x2 * y3 + x3 * y1 - x3 * y2 - x2 * y1 - x1 * y3) / 2;
         }
