488ee55c4f60265605934995b59448548f64a111
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-17 13:57:52 +0200

    Test Debt

--- Debt/Debt/Debt.cs
+++ Debt/Debt/Debt.cs
@@ -4,11 +4,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Debt
 {
     [TestClass]
-    public class UnitTest1
+    public class Debt
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(41, CalculateTotalSumToPay(10, 31));
+        }
+        float CalculateTotalSumToPay(double rent, int lateDays)
+        {
+            if (lateDays > 0 && lateDays < 11)
+                rent += rent * 0.02 * lateDays;
+            else if (lateDays > 10 && lateDays < 31)
+                rent += rent * 0.05 * lateDays;
+            else if (lateDays > 30)
+                rent += rent * 0.1 * lateDays;
+            return (float) rent;
         }
     }
 }
