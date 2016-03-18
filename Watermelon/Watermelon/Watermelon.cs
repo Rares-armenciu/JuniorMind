3438243188eaafd7c03d8e8352b402e2d286eaac
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-18 13:08:06 +0200

    Watermelon corrected

--- Watermelon/Watermelon/Watermelon.cs
+++ Watermelon/Watermelon/Watermelon.cs
@@ -16,9 +16,16 @@ namespace Watermelon
         {
             Assert.AreEqual(false, CheckWatermelonKg(21));
         }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(false, CheckWatermelonKg(2));
+        }
         Boolean CheckWatermelonKg(int watermelonWeight)
         {
-            if (watermelonWeight % 2 == 0)
+            if (watermelonWeight == 2)
+                return false;
+            else if (watermelonWeight % 2 == 0)
                 return true;
             else
                 return false;
