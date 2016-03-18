b80905d512c8672f259e89b4a11f639cca555bc8
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-18 13:16:53 +0200

    RomanNumbers first test

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -4,11 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RomanNumbers
 {
     [TestClass]
-    public class UnitTest1
+    public class RomanNumbers
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual("III", ConvertNumberToRoman(3));
+        }
+        string ConvertNumberToRoman(int number)
+        {
+            return string.Empty;
         }
     }
 }
