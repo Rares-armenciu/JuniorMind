9693975b594d4dc04cc7feda696928d4ced253bc
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-18 17:01:29 +0200

    RomanNumbers final

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -14,33 +14,24 @@ namespace RomanNumbers
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual("XIV", ConvertNumberToRoman(14));
+            Assert.AreEqual("XIII", ConvertNumberToRoman(13));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("XCIX", ConvertNumberToRoman(99));
         }
         string ConvertNumberToRoman(int number)
         {
             string romanNumber = null;
-            int[] roman = { 1, 5, 10, 50, 100 };
-            string roman2 = "IVXLC";
-            for(int i=4; i>-1; i--)
+            int[] roman = { 1, 4, 5, 9, 10, 40, 50, 90, 100 };
+            string[] roman2 = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C" };
+            for(int i=8; i>-1; i--)
             {
-                if(number/roman[i]!=0)
+                while(number/roman[i]!=0)
                 {
-                    if(number>=90)
-                    {
-                        romanNumber = "XC";
-                        number -= 90;
-                        i = 2;
-                    }
-                    if(number/roman[i]==4)
-                    {
-                        romanNumber += roman2[i];
-                        romanNumber += roman2[i + 1];
-                    }
-                    else
-                    {
-                        romanNumber += roman2[i];
-                        number -= roman[i];
-                    }
+                    romanNumber += roman2[i];
+                    number -= roman[i];
                 }
             }
             return romanNumber;
