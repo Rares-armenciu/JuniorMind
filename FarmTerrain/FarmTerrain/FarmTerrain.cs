3cf1674d06be019b86f91add94d516794c275ff9
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-18 18:03:21 +0200

    First test

--- FarmTerrain/FarmTerrain/FarmTerrain.cs
+++ FarmTerrain/FarmTerrain/FarmTerrain.cs
@@ -9,6 +9,20 @@ namespace FarmTerrain
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(2, getTerrainSize(2, 8));
+        }
+        double getTerrainSize(int addedSize, int totalArea)
+        {
+            double sqrtDelta =Math.Sqrt( Math.Pow(addedSize, 2) + 4 * totalArea);
+            return getFirstValue(sqrtDelta, addedSize);
+        }
+        double getFirstValue(double sqrtDelta, int addedSize)
+        {
+            return (sqrtDelta - addedSize) / 2;
+        }
+        double getSecondValue(double sqrtDelta, int addedSize)
+        {
+            return - (addedSize + sqrtDelta) / 2;
         }
     }
 }
