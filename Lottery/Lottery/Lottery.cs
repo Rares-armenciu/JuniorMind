bd1270fdb8e73d31d93177e1e9ab79b9c52013e5
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-21 23:24:09 +0200

    Empty Lottery project

---
+++ Lottery/Lottery/Lottery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lottery
+{
+    [TestClass]
+    public class Lottery
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
