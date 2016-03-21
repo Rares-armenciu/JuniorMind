f88bfdcb02afd978df65dc8fba8b002fcf6b9dbb
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-21 21:47:32 +0200

    Empty Prefix project

---
+++ Prefix/Prefix/Prefix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class Prefix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
