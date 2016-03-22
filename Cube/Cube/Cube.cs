27b1182cd0a08e530f8d1aab7bf20574190b618f
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-22 13:49:24 +0200

    Implemented code

--- Cube/Cube/Cube.cs
+++ Cube/Cube/Cube.cs
@@ -13,6 +13,16 @@ namespace Cube
         }
         int GetCubeNumber(int wantedNumber)
         {
+            int counter = 0;
+            for(int i=0; i<1000; i++)
+            {
+                if(Math.Pow(i, 3)%1000==888)
+                {
+                    counter++;
+                    if (counter == wantedNumber)
+                        return i;
+                }
+            }
             return 0;
         }
     }
