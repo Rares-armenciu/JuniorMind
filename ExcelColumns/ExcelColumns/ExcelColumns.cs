312311e866a0fd94e7d64496abac54801d37ebab
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-27 12:59:24 +0300

    Test for three letters is green

--- ExcelColumns/ExcelColumns/ExcelColumns.cs
+++ ExcelColumns/ExcelColumns/ExcelColumns.cs
@@ -22,25 +22,36 @@ namespace ExcelColumns
             Assert.AreEqual(53, GetNumberOfColumn("BA"));
         }
         [TestMethod]
+        public void TestForZZ()
+        {
+            Assert.AreEqual(702, GetNumberOfColumn("ZZ"));
+        }
+        [TestMethod]
         public void TestForThreeLetters()
         {
             Assert.AreEqual(703, GetNumberOfColumn("AAA"));
         }
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(728, GetNumberOfColumn("AAZ"));
+        }
         private int GetNumberOfColumn(string columnInLetters)
         {
-            int finalResult = 0;
-            int i = 0;
-            while(i<columnInLetters.Length)
+            int finalResult = 1;
+            for(int i=0; i<columnInLetters.Length; i++)
             {
-                if (i!=columnInLetters.Length-1)
+                if (i != columnInLetters.Length - 1)
                 {
-                    finalResult = (columnInLetters[i] - 'A' + 1) * 26;
-                    i++;
+                    finalResult *= (columnInLetters[i] - 'A' + 1) * 26;
+                    finalResult++;
                 }
-                finalResult += columnInLetters[i] - 'A';
-                i++;
+                else
+                {
+                    finalResult += columnInLetters[i] - 'A';
+                }
             }
-            return finalResult+1;
+            return finalResult;
         }
     }
 }
