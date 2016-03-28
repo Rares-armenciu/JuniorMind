5734d44a5e62fb2d81618b5df352e887be73ae32
Rares-armenciu <rares.armenciu@gmail.com>
2016-03-28 12:27:46 +0300

    Code for second test

--- ExcelColumnsInverted/ExcelColumnsInverted/ExcelColumnsInverted.cs
+++ ExcelColumnsInverted/ExcelColumnsInverted/ExcelColumnsInverted.cs
@@ -11,11 +11,22 @@ namespace ExcelColumnsInverted
         {
             Assert.AreEqual("A", GetColumnLetters(1));
         }
+        [TestMethod]
+        public void TestForTwoLetters()
+        {
+            Assert.AreEqual("AA", GetColumnLetters(27));
+        }
+        [TestMethod]
         string GetColumnLetters(int columnNumber)
         {
             string columnLetters = string.Empty;
             if (columnNumber < 27)
                 columnLetters = columnLetters + (char)(columnNumber + 64);
+            if(columnNumber > 26 && columnNumber <= 702)
+            {
+                columnLetters = columnLetters + (char)(columnNumber / 26 + 64);
+                columnLetters = columnLetters + (char)(columnNumber % 26 + 64);
+            }
             return columnLetters;
         }
     }
