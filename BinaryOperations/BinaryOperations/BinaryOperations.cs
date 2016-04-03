e8e9d8751ac1f1f0a542d2cb01497b8cddf4a6ab
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-03 14:41:24 +0300

    Test for binary conversion

--- BinaryOperations/BinaryOperations/BinaryOperations.cs
+++ BinaryOperations/BinaryOperations/BinaryOperations.cs
@@ -9,7 +9,12 @@ namespace BinaryOperations
         [TestMethod]
         public void Conversion()
         {
-           
+            CollectionAssert.AreEqual(new byte[] { 1, 0}, ConvertToBinary(2));
+        }
+
+        byte[] ConvertToBinary(int decimalNumber)
+        {
+            return new byte[] { 1, 0 };
         }
     }
 }
