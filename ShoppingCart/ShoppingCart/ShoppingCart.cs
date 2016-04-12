e5825931dba933e76f51d625c3239675549a5c17
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-12 13:42:54 +0300

    Struct creation

--- ShoppingCart/ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -3,8 +3,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShoppingCart
 {
+    public struct Product
+    {
+        public int numberOfProducts;
+        public double price;
+        public Product(int numberOfProducts, double price)
+        {
+            this.numberOfProducts = numberOfProducts;
+            this.price = price;
+        }
+    }
+
     [TestClass]
-    public class UnitTest1
+    public class ShoppingCart
     {
         [TestMethod]
         public void TestMethod1()
