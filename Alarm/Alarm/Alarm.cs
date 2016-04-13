cba77591aceedc47965779208f3b8106d020ea17
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-13 13:49:56 +0300

    AlarmSettings structure

--- Alarm/Alarm/Alarm.cs
+++ Alarm/Alarm/Alarm.cs
@@ -3,8 +3,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Alarm
 {
+    public struct AlarmSettings
+    {
+        public int daySetting;
+        public int hourSetting;
+        public AlarmSettings(int daySetting, int hourSetting)
+        {
+            this.daySetting = daySetting;
+            this.hourSetting = hourSetting;
+        }
+    }
     [TestClass]
-    public class UnitTest1
+    public class Alarm
     {
         [TestMethod]
         public void TestMethod1()
