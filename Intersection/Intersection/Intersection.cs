af8d6e66fa858757bb2971c3d1f63866ed2ff234
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-13 14:58:04 +0300

    Method implementation

--- Intersection/Intersection/Intersection.cs
+++ Intersection/Intersection/Intersection.cs
@@ -9,7 +9,7 @@ namespace Intersection
     }
     public struct Coordinates
     {
-        int xCoordinate, yCoordinate;
+        public int xCoordinate, yCoordinate;
         public Coordinates(int x, int y)
         {
             xCoordinate = x;
@@ -29,7 +29,50 @@ namespace Intersection
 
         Coordinates GetIntersectionPoint(Directions[] firstRoute, Directions[] secondRoute)
         {
-            return new Coordinates(0, -2);
+            int x1 = 0, y1 = 0;
+            for(int i=0; i<firstRoute.Length; i++)
+            {
+                int x2 = 0, y2 = 0;
+                switch (firstRoute[i])
+                {
+                    case Directions.Down:
+                        y1--;
+                        break;
+                    case Directions.Up:
+                        y1++;
+                        break;
+                    case Directions.Left:
+                        x1--;
+                        break;
+                    case Directions.Right:
+                        x1++;
+                        break;
+                }
+                Coordinates firstRoutePosition = new Coordinates(x1, y1);
+                for(int j=0; j<secondRoute.Length; j++)
+                {
+                    switch (secondRoute[j])
+                    {
+                        case Directions.Down:
+                            y2--;
+                            break;
+                        case Directions.Up:
+                            y2++;
+                            break;
+                        case Directions.Left:
+                            x2--;
+                            break;
+                        case Directions.Right:
+                            x2++;
+                            break;
+                    }
+                    Coordinates secondRoutePosition = new Coordinates(x2, y2);
+                    if (firstRoutePosition.Equals(secondRoutePosition))
+                        return firstRoutePosition;
+                }
+
+            }
+            return new Coordinates(0, 0);
         }
     }
 }
