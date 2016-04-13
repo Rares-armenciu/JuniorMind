902afd1b81be676de4d09ef049c994ad6e64a3ed
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-13 14:36:38 +0300

    struct and enum types created

--- Intersection/Intersection/Intersection.cs
+++ Intersection/Intersection/Intersection.cs
@@ -3,12 +3,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Intersection
 {
+    public enum Directions
+    {
+        Up, Down, Left, Right
+    }
+    public struct Coordinates
+    {
+        int xCoordinate, yCoordinate;
+        public Coordinates(int x, int y)
+        {
+            xCoordinate = x;
+            yCoordinate = y;
+        }
+    }
     [TestClass]
-    public class UnitTest1
+    public class Intersection
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Directions[] firstRoute = new Directions[] { Directions.Left, Directions.Down, Directions.Down, Directions.Right };
+            Directions[] secondRoute = new Directions[] { Directions.Down, Directions.Down, Directions.Right, Directions.Up };
+            Assert.AreEqual(new Coordinates(0, -2), GetIntersectionPoint(firstRoute, secondRoute));
+        }
+
+        Coordinates GetIntersectionPoint(Directions[] firstRoute, Directions[] secondRoute)
+        {
+            return new Coordinates(0, -2);
         }
     }
 }
