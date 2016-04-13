0d7ddcb745c85ee8ffa0cd6212b5605d474dd3a0
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-13 14:00:22 +0300

    First test

--- Alarm/Alarm/Alarm.cs
+++ Alarm/Alarm/Alarm.cs
@@ -3,22 +3,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Alarm
 {
+    public enum Weekdays
+    {
+        Mon, Tue, Wed, Thu, Fri, Sat, Sun
+    }
     public struct AlarmSettings
     {
-        public int daySetting;
+        public Weekdays daySetting;
         public int hourSetting;
-        public AlarmSettings(int daySetting, int hourSetting)
+        public AlarmSettings(Weekdays daySetting, int hourSetting)
         {
             this.daySetting = daySetting;
             this.hourSetting = hourSetting;
         }
     }
+
     [TestClass]
     public class Alarm
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AlarmIsOffMondayAtHourTen()
         {
+            AlarmSettings[] alarmSchedule = new AlarmSettings[] {new AlarmSettings(Weekdays.Mon, 6), new AlarmSettings(Weekdays.Tue, 6),
+                new AlarmSettings(Weekdays.Wed, 6), new AlarmSettings(Weekdays.Thu, 6), new AlarmSettings(Weekdays.Fri, 6),
+                new AlarmSettings(Weekdays.Sat, 8), new AlarmSettings(Weekdays.Sun, 8)};
+            Assert.AreEqual(false, CheckIfAlarmIsOn(alarmSchedule, new AlarmSettings(Weekdays.Mon, 10)));
+        }
+
+        bool CheckIfAlarmIsOn(AlarmSettings[] alarmSchedule, AlarmSettings alarmSettings)
+        {
+            return true;
         }
     }
 }
