f1319c2dc2409295c2b14dee2a5ec9c679e67467
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-18 13:05:50 +0300

    Test for small and capital letters

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -12,31 +12,56 @@ namespace Password
     public class Password
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForSmallLettersOnly()
         {
             PasswordSettings settings = PasswordSettings.smallLetters;
             char[] password = GeneratePassword(settings, 5);
-            Assert.IsTrue(CheckPasswordComponence(password));
+            Assert.IsTrue(CheckPasswordComponence(password, settings));
+        }
+        [TestMethod]
+        public void TestForSmallAndCapitalLetters()
+        {
+            PasswordSettings settings = PasswordSettings.smallLetters | PasswordSettings.capitalLetters;
+            char[] password = GeneratePassword(settings, 5);
+            Assert.IsTrue(CheckPasswordComponence(password, settings));
         }
 
-        private bool CheckPasswordComponence(char[] password)
+        private bool CheckPasswordComponence(char[] password, PasswordSettings settings)
         {
+            int counter = 0;
+            string listOfCharacters = StringCreation(settings);
             for (int i = 0; i < password.Length; i++)
-                if (password[i] < 'a' || password[i] > 'z')
-                    return false;
-            return true;
+                for(int j=0; j<listOfCharacters.Length; j++)
+                    if(password[i] == listOfCharacters[j])
+                    {
+                        counter++;
+                        break;
+                    }
+            return (counter == password.Length);
         }
 
         char[] GeneratePassword(PasswordSettings settings, int passwordLength)
         {
             Random random = new Random();
             char[] finalPassword = new char[passwordLength];
-            var chars = "abcdefghijklmnopqrstuvwxyz";
+            string chars = StringCreation(settings);
             for (int i = 0; i < finalPassword.Length; i++)
             {
                 finalPassword[i] = chars[random.Next(chars.Length)];
             }
             return finalPassword;
+        }
+
+        string StringCreation(PasswordSettings settings)
+        {
+            string smallLetters = "abcdefghijklmnopqrstuvwxyz";
+            string capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string finalString = null;
+            if ((settings & PasswordSettings.smallLetters) != 0)
+                finalString = finalString + smallLetters;
+            if ((settings & PasswordSettings.capitalLetters) != 0)
+                finalString = finalString + capitalLetters;
+            return finalString;
         }
     }
 }
