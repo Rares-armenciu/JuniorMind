b5246b8f9efc809b787ff7bc9bb767523bbfd3a8
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-21 23:32:50 +0300

    First test for full distance calculation

--- Bicycle/Bicycle/Bicycle.cs
+++ Bicycle/Bicycle/Bicycle.cs
@@ -3,12 +3,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bicycle
 {
+    public struct Rotations
+    {
+        public int second, rotations;
+        public Rotations(int x, int y)
+        {
+            second = x;
+            rotations = y;
+        }
+    }
     [TestClass]
-    public class UnitTest1
+    public class Bicycle
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Rotations[] firstCompetitor = new Rotations[] { new Rotations(1, 1), new Rotations(2, 3), new Rotations(3, 2) };
+            Rotations[] secondCompetitor = new Rotations[] { new Rotations(1, 2), new Rotations(2, 4), new Rotations(3, 1) };
+            Rotations[] thirdCompetitor = new Rotations[] { new Rotations(1, 3), new Rotations(2, 2), new Rotations(3, 1) };
+            Assert.AreEqual(6, GetFullDistance(firstCompetitor, 1));
+            Assert.AreEqual(4.2, GetFullDistance(secondCompetitor, 0.6));
+            Assert.AreEqual(4.8, GetFullDistance(thirdCompetitor, 0.8));
+        }
+
+        private int GetFullDistance(Rotations[] firstCompetitor, double wheelDiameter)
+        {
+            return 0;
         }
     }
 }
