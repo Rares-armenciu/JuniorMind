7d8c8aca5b232c00212bc227629946fd336578aa
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-22 00:05:18 +0300

    Fastest bicyclist name and second

--- Bicycle/Bicycle/Bicycle.cs
+++ Bicycle/Bicycle/Bicycle.cs
@@ -28,7 +28,7 @@ namespace Bicycle
     public class Bicycle
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForFullDistance()
         {
             Rotations[] firstCompetitor = new Rotations[] { new Rotations(1, 1), new Rotations(2, 3), new Rotations(3, 2) };
             Rotations[] secondCompetitor = new Rotations[] { new Rotations(1, 2), new Rotations(2, 4), new Rotations(3, 1) };
@@ -37,6 +37,17 @@ namespace Bicycle
             Assert.AreEqual(4.2, Math.Round(GetFullDistance(new Bicyclist("Cristi", secondCompetitor, 0.6)), 1));
             Assert.AreEqual(4.8, Math.Round(GetFullDistance(new Bicyclist("Sebi", thirdCompetitor, 0.8)), 1));
         }
+        [TestMethod]
+        public void TestForFastestBicyclist()
+        {
+            Rotations[] firstCompetitor = new Rotations[] { new Rotations(1, 1), new Rotations(2, 3), new Rotations(3, 2) };
+            Rotations[] secondCompetitor = new Rotations[] { new Rotations(1, 2), new Rotations(2, 4), new Rotations(3, 1) };
+            Rotations[] thirdCompetitor = new Rotations[] { new Rotations(1, 3), new Rotations(2, 2), new Rotations(3, 1) };
+            Bicyclist[] bicyclistList = new Bicyclist[] { new Bicyclist("Rares", firstCompetitor, 1), new Bicyclist("Cristi", secondCompetitor, 0.6),
+                                          new Bicyclist("Sebi", thirdCompetitor, 0.8)};
+            Assert.AreEqual("Cristi", GetBicyclistName(bicyclistList));
+            Assert.AreEqual(2, GetFastestBicyclist(bicyclistList, false));
+        }
 
         double GetFullDistance(Bicyclist bicyclist)
         {
@@ -44,6 +55,27 @@ namespace Bicycle
             for (int i = 0; i < bicyclist.rotationsPerSecond.Length; i++)
                 distance += bicyclist.rotationsPerSecond[i].rotations * bicyclist.wheelDiameter;
             return distance;
+        }
+        int GetFastestBicyclist(Bicyclist[] bicyclistList, bool name)
+        {
+            int bicyclist = 0, second = 0;
+            int highestSpeed = bicyclistList[0].rotationsPerSecond[0].rotations;
+            for(int i=0; i<bicyclistList.Length; i++)
+                for(int j=0; j<bicyclistList[i].rotationsPerSecond.Length; j++)
+                    if (bicyclistList[i].rotationsPerSecond[j].rotations > highestSpeed)
+                    {
+                        bicyclist = i + 1;
+                        highestSpeed = bicyclistList[i].rotationsPerSecond[j].rotations;
+                        second = bicyclistList[i].rotationsPerSecond[j].second;
+                    }
+            if(name)
+                return bicyclist;
+            return second;
+        }
+
+        string GetBicyclistName(Bicyclist[] bicyclistList)
+        {
+            return bicyclistList[GetFastestBicyclist(bicyclistList, true) - 1].name;
         }
     }
 }
