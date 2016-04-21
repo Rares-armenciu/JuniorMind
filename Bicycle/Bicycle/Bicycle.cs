100f965515d4068585e0db1aafcbceb754b2320a
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-21 23:48:31 +0300

    Bicycle refactorised

--- Bicycle/Bicycle/Bicycle.cs
+++ Bicycle/Bicycle/Bicycle.cs
@@ -12,6 +12,18 @@ namespace Bicycle
             rotations = y;
         }
     }
+    public struct Bicyclist
+    {
+        public string name;
+        public Rotations[] rotationsPerSecond;
+        public double wheelDiameter;
+        public Bicyclist(string name, Rotations[] rotations, double diameter)
+        {
+            this.name = name;
+            rotationsPerSecond = rotations;
+            wheelDiameter = diameter;
+        }
+    }
     [TestClass]
     public class Bicycle
     {
@@ -21,16 +33,16 @@ namespace Bicycle
             Rotations[] firstCompetitor = new Rotations[] { new Rotations(1, 1), new Rotations(2, 3), new Rotations(3, 2) };
             Rotations[] secondCompetitor = new Rotations[] { new Rotations(1, 2), new Rotations(2, 4), new Rotations(3, 1) };
             Rotations[] thirdCompetitor = new Rotations[] { new Rotations(1, 3), new Rotations(2, 2), new Rotations(3, 1) };
-            Assert.AreEqual(6, GetFullDistance(firstCompetitor, 1));
-            Assert.AreEqual(4.2, Math.Round(GetFullDistance(secondCompetitor, 0.6), 1));
-            Assert.AreEqual(4.8, Math.Round(GetFullDistance(thirdCompetitor, 0.8), 1));
+            Assert.AreEqual(6, GetFullDistance(new Bicyclist("Rares", firstCompetitor, 1)));
+            Assert.AreEqual(4.2, Math.Round(GetFullDistance(new Bicyclist("Cristi", secondCompetitor, 0.6)), 1));
+            Assert.AreEqual(4.8, Math.Round(GetFullDistance(new Bicyclist("Sebi", thirdCompetitor, 0.8)), 1));
         }
 
-        double GetFullDistance(Rotations[] competitor, double wheelDiameter)
+        double GetFullDistance(Bicyclist bicyclist)
         {
             double distance = 0;
-            for (int i = 0; i < competitor.Length; i++)
-                distance += competitor[i].rotations * wheelDiameter;
+            for (int i = 0; i < bicyclist.rotationsPerSecond.Length; i++)
+                distance += bicyclist.rotationsPerSecond[i].rotations * bicyclist.wheelDiameter;
             return distance;
         }
     }
