66d4fe5df1abef6760c6a0f37589e33d39c3924f
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-28 18:40:26 +0300

    Test for one more complex case

--- ServiceCenter/ServiceCenter/ServiceCenter.cs
+++ ServiceCenter/ServiceCenter/ServiceCenter.cs
@@ -8,11 +8,15 @@ namespace ServiceCenter
     public class ServiceCenter
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleTest()
         {
             CollectionAssert.AreEqual(new int[] { 3, 2, 1 }, SetPrioritiesInOrder(new string[] { "Low", "Medium", "High" }));
         }
-
+        [TestMethod]
+        public void ComplexTest()
+        {
+            CollectionAssert.AreEqual(new int[] { 4, 6, 2, 7, 1, 3, 5}, SetPrioritiesInOrder(new string[] { "Low", "Medium", "Low", "High", "Low", "High", "Medium" }));
+        }
         int[] SetPrioritiesInOrder(string[] v)
         {
             RepairOrder repair = new RepairOrder(v);
