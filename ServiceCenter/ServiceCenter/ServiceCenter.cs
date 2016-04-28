bfe1e7ffc4666583b3978f99315b70067eddac0f
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-28 18:36:00 +0300

    SimpleTest implementation

--- ServiceCenter/ServiceCenter/ServiceCenter.cs
+++ ServiceCenter/ServiceCenter/ServiceCenter.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ServiceCenter
 {
     [TestClass]
-    public class UnitTest1
+    public class ServiceCenter
     {
         [TestMethod]
         public void TestMethod1()
         {
+            CollectionAssert.AreEqual(new int[] { 3, 2, 1 }, SetPrioritiesInOrder(new string[] { "Low", "Medium", "High" }));
+        }
+
+        int[] SetPrioritiesInOrder(string[] v)
+        {
+            RepairOrder repair = new RepairOrder(v);
+            return repair.SortPriorities();
         }
     }
 }
