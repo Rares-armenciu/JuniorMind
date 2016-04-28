2d1df1c4b0093bebb9bbff7a7ba0aeaa1ec5f56a
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-28 15:29:37 +0300

    BubbleSort class

---
+++ LotteryDraw/LotteryDraw/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryDraw
+{
+    public partial class BubbleSort : Component
+    {
+        private int[] numbersToSort;
+        public BubbleSort(int[] numberList)
+        {
+            numbersToSort = numberList;
+        }
+        
+        public int[] SortNumbers()
+        {
+            bool swap = true;
+            int auxiliary;
+            while(swap==true)
+            {
+                swap = false;
+                for (int i = 0; i < numbersToSort.Length - 1; i++)
+                    if (numbersToSort[i] > numbersToSort[i + 1])
+                    {
+                        auxiliary = numbersToSort[i];
+                        numbersToSort[i] = numbersToSort[i + 1];
+                        numbersToSort[i + 1] = auxiliary;
+                        swap = true;
+                    }
+            }
+            return numbersToSort;
+            
+        }
+    }
+}
