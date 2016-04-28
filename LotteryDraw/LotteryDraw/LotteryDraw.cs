935586d16ee2f6c3478bde5c243782050cc111e4
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-28 15:29:56 +0300

    First test and result

--- LotteryDraw/LotteryDraw/LotteryDraw.cs
+++ LotteryDraw/LotteryDraw/LotteryDraw.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LotteryDraw
 {
     [TestClass]
-    public class UnitTest1
+    public class LotteryDraw
     {
         [TestMethod]
         public void TestMethod1()
         {
+            int[] expectedNumbers = new int[] { 1, 2, 3 };
+            CollectionAssert.AreEqual(expectedNumbers, SortArrayOfNumbers(new int[] { 3, 2, 1 }));
+        }
+
+        private int[] SortArrayOfNumbers(int[] numberList)
+        {
+            BubbleSort sort = new BubbleSort(numberList);
+            return sort.SortNumbers();
         }
     }
 }
