b170389ba56762eb00811a00a7e12fdbca0fed29
Rares-armenciu <rares.armenciu@gmail.com>
2016-04-28 15:33:03 +0300

    Second test

--- LotteryDraw/LotteryDraw/LotteryDraw.cs
+++ LotteryDraw/LotteryDraw/LotteryDraw.cs
@@ -7,10 +7,16 @@ namespace LotteryDraw
     public class LotteryDraw
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleTest()
         {
             int[] expectedNumbers = new int[] { 1, 2, 3 };
             CollectionAssert.AreEqual(expectedNumbers, SortArrayOfNumbers(new int[] { 3, 2, 1 }));
+        }
+        [TestMethod]
+        public void MoreComplexTest()
+        {
+            int[] expectedNumbers = new int[] { 5, 7, 9, 16, 20, 32, 55, 62, 76, 91, 100 };
+            CollectionAssert.AreEqual(expectedNumbers, SortArrayOfNumbers(new int[] { 100, 20, 5, 32, 55, 9, 62, 91, 76, 7, 16 }));
         }
 
         private int[] SortArrayOfNumbers(int[] numberList)
