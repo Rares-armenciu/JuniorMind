634a26f7d71e9f85e5bac57c639d7f16e4ee00bc
Rares-armenciu <rares.armenciu@gmail.com>
2016-08-02 16:12:14 +0300

    Hashtable

---
+++ HashTable/HashTable/HashTableTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
